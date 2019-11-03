dfcc6e458633e6a5feaf22aca8a7619c2b410706
ruixiang.chen <ruixiang.chen@microfocus.com>
2019-11-03 21:48:01 +0800

    update UI common

---
+++ public/javascripts/IViewUpdater.ts
@@ -0,0 +1,3 @@
+export interface IViewUpdater {
+    updateUI();
+}
--- public/javascripts/view_index.ts
+++ public/javascripts/view_index.ts
@@ -1,8 +1,25 @@
 import {View_index_tasks} from "./view_index_tasks.js"
-export class View_index {
-    static getJobs():View_index_tasks[]{
+import {IViewUpdater} from "./IViewUpdater.js";
+export class View_index implements IViewUpdater{
+    getJobs():View_index_tasks[]{
         return[
             new View_index_tasks("股票名称更新","股票名称更新",1,"未开始")
         ];
+    }
+
+    updateUI() {
+        let jobs = this.getJobs();
+        for (let job of jobs){
+            console.log("get UI Update successfully");
+        }
+    }
+
+    private static instance:View_index;
+
+    static getInstance():View_index{
+        if (!View_index.instance) {
+            View_index.instance = new View_index();
+        }
+        return View_index.instance;
     }
 }
